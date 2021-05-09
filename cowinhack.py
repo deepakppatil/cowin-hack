5a4f1905ae3caacd8c211d12ca85a1d4355e4286
Deepak Patil <deepak.patil@traydstream.com>
2021-05-09 20:58:59 +0530

    enhancements/features added

--- cowinhack.py
+++ cowinhack.py
@@ -2,75 +2,95 @@ import os
 import time
 import click
 import traceback
-
 from tabulate import tabulate
 from time import sleep
 from datetime import datetime
 from crawler import crawler
 
+
 __author__ = "Deepak Patil"
+
+_headers=['Name', 'Capacity', 'Vaccine', 'PinCode','Address']
+
 
 @click.group()
 def main():
 	"""
-	Simple CLI for starting crawler on co-win to find the available spots, ctrl + C to exit
+	LI for starting crawler on co-win to find the available spots, ctrl + C to exit
 	"""
 	pass
 
-def execute(delay, task, show, pincode, announce):
+
+def execute(delay, task, show_available, pincode, district, speak_on_available, console):
 	next_time = time.time() + delay
 	while True:
 		time.sleep(max(0, next_time - time.time()))
 		try:
-			task(show, pincode, announce)
+			task(show_available, pincode, district, speak_on_available, console)
 		except Exception:
 			traceback.print_exc()
 			click.secho('Problem while executing repetitive task.', fg='red', bold=True) 
 		# skip tasks if we are behind schedule:
 		next_time += (time.time() - next_time) // delay * delay + delay
 
+
 def say(msg = "Finish", voice = "Victoria"):
     os.system(f'say -v {voice} {msg}')
 
-def run(show, pincode, announce):
+
+def run(show_available, pincode, district, speak_on_available, console):
 	msg = "fetching data form cowin for {0}....".format(datetime.today().strftime('%d-%B-%Y %H:%M:%S'))
 	click.secho(msg, fg='yellow', bold=True) 
 	date = datetime.today().strftime('%d-%m-%Y')
-	output = crawler(show, pincode, announce).start_process(date)
-	if len(output) > 0:
-		click.secho(tabulate(output, headers=['Name', 'Capacity', 'Vaccine', 'Address']), fg='yellow', bold=True)
+	output = crawler(show_available, pincode, district).process(date)
+
+	slot_found = False
+	for avail in output:
+		if avail[1] > 0:
+			slot_found = True
+			break;
+
+	if slot_found or console:
+		click.secho(tabulate(output, headers=_headers), fg='yellow', bold=True)
+
+	if slot_found:
 		for i in range(5):
 			sleep(2)
-			say("Great! Slot found, book the appointment", "Alex")
+			say("Slot found, book the appointment", "Alex")
 	else:
 		click.secho("No slot found... will try again!", fg='red', bold=True)
-		say("No slot found!")
+		if speak_on_available is False:
+			say("No slots found!")
 
 
 
 @main.command(help='start the crawler')
 @click.option('-i', '--interval', required=True, type=int, 
 			  help='Interval in seconds.')
-@click.option('-t', '--show/--no-show', default=False, 
-			  help='Track availablilty of region even if the available capacity is zero.')
-@click.option('-p','--pincode', required=True, type=int,
-			  help='Provide pincide to start the crawler.')
-@click.option('-t', '--announce/--no-announce', default=False, 
-			  help='Announce loudly when available.')
-def start(interval, show, pincode, announce):
+@click.option('-s', '--show-available', is_flag=True, default=False, help='List all centers only if slots are available.')
+@click.option('-p','--pincode', type=int, help='Provide pincide to start the crawler.')
+@click.option('-d','--district', type=int, default=392,
+			  help='Provide district code to filter the crawler, default is Thane')
+@click.option('-a', '--speak-on-available', default=False, is_flag=True,
+			  help='Announce only when slots are available.')
+@click.option('-c', '--console/--no-console', default=False, is_flag=True,
+			  help='Print list of centers even if slots not available.')
+def start(interval, show_available, pincode, district, speak_on_available, console):
 	click.secho('initialising crawler....', fg='cyan') 
-	execute(interval, run, show, pincode, announce)
+	execute(interval, run, show_available, pincode, district, speak_on_available, console)
 
 
 @main.command(help='List down all the centers with address')
 @click.option('-p','--pincode', type=int, help='Provide pincide to list the hospitals.')
-def list(pincode):
-
-	click.secho('listing all the centers....', fg='yellow', bold=True)
-	date = datetime.today().strftime('%d-%m-%Y')
-	output = crawler(False, pincode).list_centers(date)
+@click.option('-d','--district', type=int, default=392,
+			  help='Provide district code to filter the crawler, default is thane')
+@click.option('-s', '--show-available', is_flag=True, default=False, help='List all centers only if slots are available.')
+def list(pincode, district, show_available):
+	click.secho('listing {} centers for PinCode: {} and District: {}'.format('only the available' if show_available else 'all the', pincode, district), 
+			fg='yellow', bold=True)
+	output = crawler(show_available, pincode, district).process(datetime.today().strftime('%d-%m-%Y'))
 	if len(output) > 0:
-		click.secho(tabulate(output, headers=['PinCode', 'Center Id', 'Name & Address']), fg='yellow', bold=True)
+		click.secho(tabulate(output, headers=_headers), fg='yellow', bold=True)
 	else:
 		click.secho("No centers available...", fg='red', bold=True)
 
--- crawler.py
+++ crawler.py
@@ -14,53 +14,37 @@ log = logging.getLogger('__name__')
 
 class crawler:
 
-    # base url
-    base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=392&date="
-
-    def __init__(self, show, pincode, announce=False):
+    def __init__(self, show_available, pincode, district):
         log.debug("__inside__")
-        self.show = show
+        self.show = show_available
         self.pincode = pincode
-        self.announce = announce
-        
-        
-    def start_process(self, date):
-        """
-        function the data from cowin
-        """
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
-        result = requests.get("{0}{1}".format(self.base, date), headers=headers)
-        if (result.status_code == 200):
-            return self.churn(result.json())
-        else:
-            log.error("Got {} response..".format(result.status_code))
-            return []
-    
-    def churn(self, data):
-        output = []
-        for field in data['centers']:
-            if field['pincode'] == self.pincode:
-                for session in field['sessions']:
-                    record = [field['name'], 
-                                  session['available_capacity'], 
-                                  session['vaccine'], 
-                                  "{}, {}".format(field['block_name'], field['address'])]
-                    if session['available_capacity'] > 0 or self.show:
-                        output.append(record)
-                        
-        return output
+        self.district = district
+        self.base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date=".format(district)
 
-    def list_centers(self, date):
-        """
-        function the data from cowin
-        """
+    def process(self, date):
         output = []
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
         result = requests.get("{0}{1}".format(self.base, date), headers=headers)
         if (result.status_code == 200):
             data = result.json()
+            if not 'centers' in data or len(data['centers']) == 0:
+                return output
             for field in data['centers']:
-                if field['pincode'] == self.pincode or self.pincode is None:
-                    addr = "{}: {}, {}".format(field['name'], field['block_name'], field['address'])
-                    output.append([field['pincode'], field['center_id'], addr])
-            return output
+                record = None
+                for session in field['sessions']:
+                    if self.pincode is None or field['pincode'] == self.pincode:
+                        record = [field['name'], 
+                                  session['available_capacity'], 
+                                  session['vaccine'], 
+                                  field['pincode'],
+                                  "{}, {}".format(field['block_name'], field['address'])]
+                    if record is not None:
+                        if self.show is False:
+                            output.append(record)
+                        else:
+                            if session['available_capacity'] > 0:
+                                output.append(record)
+        else:
+            log.error("Got {} response..".format(result.status_code))
+                        
+        return output
