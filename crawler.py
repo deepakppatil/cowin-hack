b9d078515961ca861b8bc2466cd12dce9ed33171
Deepak Patil <deepak.patil@traydstream.com>
2021-05-20 19:37:28 +0530

    token enhancement, public api support if no token specified

--- crawler.py
+++ crawler.py
@@ -12,29 +12,31 @@ from time import sleep
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 log = logging.getLogger('__name__')
 urllib3_logger = logging.getLogger('urllib3')
-urllib3_logger.setLevel(logging.DEBUG)
+urllib3_logger.setLevel(logging.CRITICAL)
 
 class crawler:
 
 	def __init__(self, pincode, district):
 		self.pincode = pincode
 		self.district = district
-		# self.base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date=".format(district)
-		self.base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={}&date=".format(district)
 
-		# 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIyZGI3M2JmYi0yMjc1LTRjYzgtYmY0MS1jNzcxYjcwNGIyZWEiLCJ1c2VyX2lkIjoiMmRiNzNiZmItMjI3NS00Y2M4LWJmNDEtYzc3MWI3MDRiMmVhIiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo5OTMwODcwMjk5LCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjM5Mzg2NTMyOTEwNzcsInNlY3JldF9rZXkiOiJiNWNhYjE2Ny03OTc3LTRkZjEtODAyNy1hNjNhYTE0NGYwNGUiLCJ1YSI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzYpIEFwcGxlV2ViS2l0LzYwNS4xLjE1IChLSFRNTCwgbGlrZSBHZWNrbykgVmVyc2lvbi8xNC4wLjMgU2FmYXJpLzYwNS4xLjE1IiwiZGF0ZV9tb2RpZmllZCI6IjIwMjEtMDUtMTlUMjA6NDk6MDIuMjM0WiIsImlhdCI6MTYyMTQ1NzM0MiwiZXhwIjoxNjIxNDU4MjQyfQ.1upIg-cf0fVlwLIk1Mp0nRjOnBVm3l4YloODekpv3dU'
-	
 	def process(self, date, min_age, max_age, show_available, token):
 
 		output = []
 
 		try:
-			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
-					   'Authorization': 'Bearer {}'.format(token),
-					   'referer': 'https://selfregistration.cowin.gov.in/',
-					   'origin': 'https://selfregistration.cowin.gov.in'
-			}
-			url = "{0}{1}".format(self.base, date)
+			base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date=".format(self.district)
+			if token is None:
+				headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
+			else:
+				headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+						   'Authorization': 'Bearer {}'.format(token),
+						   'referer': 'https://selfregistration.cowin.gov.in/',
+						   'origin': 'https://selfregistration.cowin.gov.in'
+				}
+				base = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={}&date=".format(self.district)
+
+			url = "{0}{1}".format(base, date)
 			log.debug(url)
 
 			try:
